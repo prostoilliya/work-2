e10a1de8e42a219aa89362e2651cd883fa525cd6
illia.kleshchev@gmail.com <illia.kleshchev@gmail.com>
2021-06-29 12:30:34 +0300

    Initial commit

--- app.py
+++ app.py
@@ -3,68 +3,32 @@ from cryptography.fernet import Fernet
 
 
 app = Flask(__name__)
-
+#add the key
 key = Fernet.generate_key()
   
-# value of key is assigned to a variable
+
 f = Fernet(key)
   
-# the plaintext is converted to ciphertext
+
 token = f.encrypt(b"welcome to geeksforgeeks")
 d = f.decrypt(token)
 #http://127.0.0.1:5000/
 
-@app.route('/')
+@app.route('/encrypt')
 def index():
     return render_template(
         'index.html' ,
-        # d = f.decrypt(token),
         name=token
 
     )
     
-@app.route('/d')
+@app.route('/decrypt')
 def index1():
     return render_template(
         'index.html' ,
         name3 = d
         
     )
-    
-# @app.route('/encrypt?string=<string_to_encrypt>')
-# def index1():
-#     return render_template(
-#         'index.html' ,
-#         name3='adf9420-12fnkadl;f=4fjmqe' ,
-#     )
-    
-
-
-#@app.route('/encrypt?string=<string_to_encrypt>')
-#def get_index():
-#    return "<strong>Its index page<strong>"
-
-
-
-@app.route('/catalog')
-def get_catalog():
-    return render_template('catalog.html')
-    
-    
-
-@app.route('/row/<item>')
-def get_row(item='Default value'):
-    return render_template('item.html', item=item)
-    
-    
-    
-@app.route('/product')
-def get_product_by_args():
-    item = request.args.get('item', 'Default value')
-
-    return render_template('item.html', item=item)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
